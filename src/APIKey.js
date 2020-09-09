4e40af4c9e5c182ce53e91942dbf4a30f1ded812
Kartik Gupta <thisiskartikgupta2019@gmail.com>
2020-09-09 17:20:11 +0530

    Version 1.0

---
+++ src/APIKey.js
@@ -0,0 +1,3 @@
+module.exports = {
+  APIKey: "5fa598dd",
+};
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <div>
+      <nav className="navbar navbar-light bg-dark mb-5">
+        <div className="container">
+          <div className="navbar-header">
+            <Link className="navbar-brand text-white text-lg brand-text" to="/">
+              ICAS || Movies
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
